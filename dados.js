3842924428f93149214a9b4d1da7943bda8c2758
Luigi Scola <luigiscola.info@gmail.com>
2026-02-18 17:22:46 -0300

    t

--- dados.js
+++ dados.js
@@ -16,21 +16,19 @@ window.dados = [
   { produto: "MDM On-Premise", edicao: "Standard", horas: 8, caminho: "MDM On-Premise/Mobile Device Manager Plus - Standard.docx" },
   { produto: "MDM On-Premise", edicao: "Professional", horas: 12, caminho: "MDM On-Premise/Mobile Device Manager Plus - Professional.docx" },
 
-  { produto: "Endpoint Central Plus On-Premise", edicao: "Enterprise", horas: 14, caminho: "Endpoint Central Plus On-Premise/Endpoint Central Plus On-Premise - Enterprise.docx" },
-  { produto: "Endpoint Central Plus On-Premise", edicao: "Professional", horas: 12, caminho: "Endpoint Central Plus On-Premise/Endpoint Central Plus On-Premise - Professional.docx" },
-  { produto: "Endpoint Central Plus On-Premise", edicao: "Security", horas: 24, caminho: "Endpoint Central Plus On-Premise/Endpoint Central Plus On-Premise - Security.docx" },
-
-  { produto: "Endpoint Central Plus Cloud", edicao: "Enterprise", horas: 12, caminho: "Endpoint Central Plus Cloud/Endpoint Central Plus Cloud - Enterprise.docx" },
-  { produto: "Endpoint Central Plus Cloud", edicao: "UEM", horas: 12, caminho: "Endpoint Central Plus Cloud/Endpoint Central Plus Cloud - UEM.docx" },
-  { produto: "Endpoint Central Plus Cloud", edicao: "Security", horas: 24, caminho: "Endpoint Central Plus Cloud/Endpoint Central Plus Cloud - Security.docx" },
-
-  { produto: "Endpoint Central Cloud", edicao: "Enterprise", horas: 12, caminho: "Endpoint Central Cloud/Endpoint Central Cloud - Enterprise.docx" },
-  { produto: "Endpoint Central Cloud", edicao: "UEM", horas: 12, caminho: "Endpoint Central Cloud/Endpoint Central Cloud - UEM.docx" },
-  { produto: "Endpoint Central Cloud", edicao: "Security", horas: 24, caminho: "Endpoint Central Cloud/Endpoint Central Cloud - Security.docx" },
+  { produto: "Endpoint Central Cloud", edicao: "Enterprise", horas: 14, caminho: "Endpoint Central Cloud/Endpoint Central Plus Cloud - Enterprise.docx" },
+  { produto: "Endpoint Central Cloud", edicao: "Professional", horas: 12, caminho: "Endpoint Central Cloud/Endpoint Central Plus Cloud - Professional.docx" },
+  { produto: "Endpoint Central Cloud", edicao: "UEM", horas: 6, caminho: "Endpoint Central Cloud/Endpoint Central Plus Cloud - UEM.docx" },
+  { produto: "Endpoint Central Cloud", edicao: "Security", horas: 24, caminho: "Endpoint Central Cloud/Endpoint Central Plus Cloud - Security.docx" },
 
   { produto: "Endpoint Central On-Premise", edicao: "Enterprise", horas: 14, caminho: "Endpoint Central On-Premise/Endpoint Central On-Premise - Enterprise.docx" },
   { produto: "Endpoint Central On-Premise", edicao: "Professional", horas: 12, caminho: "Endpoint Central On-Premise/Endpoint Central On-Premise - Professional.docx" },
+  { produto: "Endpoint Central On-Premise", edicao: "UEM", horas: 16, caminho: "Endpoint Central On-Premise/Endpoint Central Plus Cloud - UEM.docx" },
   { produto: "Endpoint Central On-Premise", edicao: "Security", horas: 24, caminho: "Endpoint Central On-Premise/Endpoint Central On-Premise - Security.docx" },
+
+  { produto: "Endpoint Central MSP", edicao: "Cloud Enterprise", horas: 16, caminho: "Endpoint Central MSP/Endpoint Central MSP Cloud - Enterprise.docx" },
+  { produto: "Endpoint Central MSP", edicao: "Cloud Professional", horas: 14, caminho: "Endpoint Central MSP/Endpoint Central MSP Cloud - Professional.docx" }
+  { produto: "Endpoint Central MSP", edicao: "On-Premise Enterprise", horas: 16, caminho: "Endpoint Central MSP/Endpoint Central MSP On-Prem - Enterprise.docx" },
 
   { produto: "OpManager", edicao: "Standard", horas: 10, caminho: "OpManager/OpManager - Standard.docx" },
   { produto: "OpManager", edicao: "Professional", horas: 16, caminho: "OpManager/OpManager - Professional.docx" },
@@ -41,15 +39,14 @@ window.dados = [
   { produto: "OpManager Plus", edicao: "Enterprise", horas: 24, caminho: "OpManager Plus/OpManager Plus - Enterprise.docx" },
   { produto: "OpManager Plus", edicao: "MSP", horas: 24, caminho: "OpManager Plus/OpManager Plus - MSP.docx" },
 
-  { produto: "OpManager MSP", edicao: "Standard", horas: 14, caminho: "OpManager MSP/OpManager MSP - Standard.docx" },
-  { produto: "OpManager MSP", edicao: "Professional", horas: 20, caminho: "OpManager MSP/OpManager MSP - Professional.docx" },
-  { produto: "OpManager MSP", edicao: "Enterprise", horas: 24, caminho: "OpManager MSP/OpManager MSP - Enterprise.docx" },
-  { produto: "OpManager MSP", edicao: "MSP", horas: 24, caminho: "OpManager MSP/OpManager MSP - MSP.docx" },
+  { produto: "OpManager MSP", edicao: "-", horas: 20, caminho: "OpManager MSP/OpManager MSP - Professional.docx" },
 
   { produto: "Applications Manager", edicao: "Professional", horas: 14, caminho: "Applications Manager/Applications Manager - Professional.docx" },
   { produto: "Applications Manager", edicao: "Enterprise", horas: 16, caminho: "Applications Manager/Applications Manager - Enterprise.docx" },
 
-  { produto: "Site24x7", edicao: "Enterprise", horas: 14, caminho: "Site24x7/Site24x7 - Enterprise.docx" },
+  { produto: "Site24x7", edicao: "Elite", horas: 24, caminho: "Site24x7/Escopo de Implantação - Site 24x7 - Elite Edition.docx" },
+  { produto: "Site24x7", edicao: "Professional", horas: 20, caminho: "Site24x7/Escopo de Implantação - Site 24x7 - Professional Edition.docx" },
+  { produto: "Site24x7", edicao: "MSP", horas: 26, caminho: "Site24x7/Escopo de Implantação - Site 24x7 - MSP Edition.docx" },
 
   { produto: "ServiceDesk Plus", edicao: "Standard", horas: 12, caminho: "ServiceDesk Plus/ServiceDesk Plus - Standard.docx" },
   { produto: "ServiceDesk Plus", edicao: "Professional", horas: 18, caminho: "ServiceDesk Plus/ServiceDesk Plus - Professional.docx" },
@@ -68,7 +65,6 @@ window.dados = [
   
   { produto: "ADSelfService Plus", edicao: "Standard", horas: 10, caminho: "ADSelfService Plus/ADSelfService Plus - Standard.docx" },
   { produto: "ADSelfService Plus", edicao: "Professional", horas: 12, caminho: "ADSelfService Plus/ADSelfService Plus - Professional.docx" },
-  { produto: "ADSelfService Plus", edicao: "Enterprise", horas: 14, caminho: "ADSelfService Plus/ADSelfService Plus - Enterprise.docx" },
 
   { produto: "Analytics Plus", edicao: "Professional", horas: 14, caminho: "Analytics Plus/Analytics Plus - Professional.docx" },
   { produto: "Analytics Plus", edicao: "Enterprise", horas: 16, caminho: "Analytics Plus/Analytics Plus - Enterprise.docx" },
