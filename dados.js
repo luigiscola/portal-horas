4ac2438a1f2c7b3012e933c8696297e56f9c1bbc
Guilherme Princz <91706194+GuiPrincz@users.noreply.github.com>
2026-03-05 11:49:36 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -80,7 +80,7 @@ window.dados = [
   { produto: "PAM 360 MSP", edicao: "-", horas: 16, caminho: "PAM 360 MSP/Escopo de Implantação - PAM360 - MSP.docx" },
   { produto: "KeyManager Plus", edicao: "-", horas: 6, caminho: "Key Manager Plus/Escopo de Implantação - Key Manager Plus.docx" },
 
-  { produto: "Firewall Analyzer", edicao: "-", horas: 6, caminho: "Firewall Analyzer/Escopo de Implantação - Firewall Analyzer - Professional Edition1.docx" },
+  { produto: "Firewall Analyzer", edicao: "Professional", horas: 6, caminho: "Firewall Analyzer/Escopo de Implantação - Firewall Analyzer - Professional Edition1.docx" },
 
   { produto: "M365 Management", edicao: "Standard", horas: 6, caminho: "M365 Management/Escopo_de_Implantação_-_M365MP_Standard.docx" },
   { produto: "M365 Management", edicao: "Professional", horas: 6, caminho: "M365 Management/Escopo_de_Implantação_-_M365MP_Professional.docx" },
@@ -90,15 +90,15 @@ window.dados = [
   { produto: "Mobile Device Management Plus MSP", edicao: "Cloud Standard", horas: 30, caminho: "Mobile Device Management Plus MSP/MDM MSP Cloud - Standard.docx" },
   { produto: "Mobile Device Management Plus MSP", edicao: "Cloud Professional", horas: 12, caminho: "Mobile Device Management Plus MSP/MDM MSP Cloud - Professional.docx" },
   
-  { produto: "NetFlow Analyzer", edicao: "-", horas: 12, caminho: "NetFlow Analyzer/Escopo de Implantação - Netflow Analyzer - Professional Edition.docx" },
+  { produto: "NetFlow Analyzer", edicao: "Professional", horas: 12, caminho: "NetFlow Analyzer/Escopo de Implantação - Netflow Analyzer - Professional Edition.docx" },
 
-  { produto: "Network Configuration Manager", edicao: "-", horas: 12, caminho: "Network Configuration Manager/Escopo de Implantação - NCM - Professional Edition - Copia.docx" },
+  { produto: "Network Configuration Manager", edicao: "Professional", horas: 12, caminho: "Network Configuration Manager/Escopo de Implantação - NCM - Professional Edition - Copia.docx" },
 
   { produto: "Patch Manager Plus On-Premise", edicao: "Professional", horas: 12, caminho: "Patch Manager Plus/Patch Manager Plus On-Prem Professional.docx" },
   { produto: "Patch Manager Plus On-Premise", edicao: "Enterprise", horas: 18, caminho: "Patch Manager Plus/Patch Manager Plus On-Prem Enterprise .docx" },
   { produto: "Patch Manager Plus Cloud", edicao: "Professional", horas: 30, caminho: "Patch Manager Plus Cloud/Patch Manager Plus Cloud Professional.docx" },
   { produto: "Patch Manager Plus Cloud", edicao: "Enterprise", horas: 12, caminho: "Patch Manager Plus Cloud/Patch Manager Plus Cloud Enterprise .docx" },
 
-  { produto: "Recovery Manager Plus", edicao: "-", horas: 12, caminho: "Recovery Manager Plus/Escopo_de_Implantação_-_RecoveryManagerPlus_Standard.docx" },
+  { produto: "Recovery Manager Plus", edicao: "Standard", horas: 12, caminho: "Recovery Manager Plus/Escopo_de_Implantação_-_RecoveryManagerPlus_Standard.docx" },
 
 ];
